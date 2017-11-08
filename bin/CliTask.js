4e5ca9d887ec4308bddb52294c37197b8648772f
Alex Shnayder <alex@shnayder.pro>
2017-11-09 00:37:00 +0700

    Outline the conflict handler

--- bin/CliTask.js
+++ bin/CliTask.js
@@ -1,6 +1,7 @@
 const template = require('lodash.template')
 const change = require('gulp-change')
 const rename = require('gulp-simple-rename')
+const conflict = require('gulp-conflict')
 const FileTask = require('../src/FileTask')
 
 
@@ -10,12 +11,23 @@ function templateHandler(files, options) {
     .pipe(rename((path) => template(path)(options)))
 }
 
+function conflictHandler(files, options) {
+  return files.pipe(conflict(options.dest))
+}
+
 
 class CliTask extends FileTask {
   constructor(config, cliConfig) {
     super(config)
 
     this.cliConfig = cliConfig
+
+    this.command.lifecycle.hook({
+      event: 'handle',
+      tags: ['handleCommand'],
+      goesAfter: ['handleCommand'],
+    }, this._makeHandler(conflictHandler))
+
     this.command.lifecycle.hook({
       event: 'handle',
       tags: ['handleCommand'],
